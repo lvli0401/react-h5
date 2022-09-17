6b494dfa3b49d340b1e33e6191762a8eb736f9df
currycheng777 <currycheng@163.com>
2022-09-17 20:59:49 +0800

    [add]: 个人中心已经微信授权登录

--- src/pages/UserCenter/index.tsx
+++ src/pages/UserCenter/index.tsx
@@ -1,13 +1,83 @@
-import React, { useState } from "react";
-import { LeftOutline } from "antd-mobile-icons";
+import React, { useState, useEffect } from "react";
+import { LeftOutline, RightOutline } from "antd-mobile-icons";
+import { useNavigate } from 'react-router-dom';
 import styles from "./index.module.scss";
+import { checkRedirect } from '@utils/index';
 
-const Record: React.FC<any> = () => {
+const list = [
+  {
+    id: 1,
+    icon: '',
+    name: '预约记录',
+    path: '/record',
+  }, {
+    id: 2,
+    icon: '',
+    name: '上传风采信息',
+    path: '',
+  }, {
+    id: 3,
+    icon: '',
+    name: '场馆预约审核列表',
+    path: '/venueBooking',
+  }, {
+    id: 4,
+    icon: '../../assets/images/img-活动预约审核@2x.png',
+    name: '活动预约审核列表',
+    path: '',
+  }, {
+    id: 5,
+    icon: '',
+    name: '活动报名二维码生成',
+    path: '',
+  },
+
+]
+const Record: React.FC<any> = (props: any) => {
+  const navigate = useNavigate();
+  const jump2Page = (path: string) => {
+    navigate(path);
+  };
+  const goBack = () => {
+    navigate(-1);
+  }
+
+  const checkRedirec = async () => {
+    checkRedirect();
+  }
+
+  useEffect(() => {
+    checkRedirec();
+  }, [])
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      <div className={styles.header} onClick={goBack}>
         <LeftOutline fontSize={16} />
         <span className={styles.title}>个人中心</span>
+      </div>
+      <div className={styles.loginBox}>
+        <div className={styles.rightBar}>
+          管理账号登录
+          <RightOutline />
+        </div>
+        <div className={styles.avatar}></div>
+        <div className={styles.loginText}>登录</div>
+      </div>
+      <div className={styles.card}>
+        {
+          list.map(i => (
+            <div
+              className={styles.item}
+              key={i.id}
+              onClick={() => jump2Page(i.path)}
+            >
+              <img src={i.icon} />
+              <span className={styles.text}>{i.name}</span>
+            </div>
+          ))
+        }
+
       </div>
     </div>
   );
---
+++ src/utils/index.ts
@@ -0,0 +1,65 @@
+import storage from "./storage";
+const config = {
+  appid: 'wx858f95d036ff8738', // 测试公众号
+  // appid: 'wxe5be0ea7c9f3ab15', // 南桥公众号id
+};
+
+
+interface genOauthConfig {
+  appid: string;
+}
+
+/**
+ * 获取重定位的 OAuth 路径
+ * @returns {string}
+ */
+const generateOAuthUrl = (config: genOauthConfig) => {
+  const [redirectUri] = window.location.href.split('#');
+
+  const searchObj = {
+    appid: config.appid,
+    redirect_uri: encodeURIComponent(redirectUri),
+    response_type: 'code',
+    scope: 'snsapi_userinfo',
+    state: 'A1',
+  };
+
+  const search = Object.entries(searchObj)
+    .map((entry) => {
+      const [key, value] = entry;
+      return `${key}=${value}`;
+    })
+    .join('&');
+
+  return `https://open.weixin.qq.com/connect/oauth2/authorize?${search}#wechat_redirect`;
+};
+
+
+const getQueryString = (name: string) => {
+  const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  const r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return decodeURIComponent(r[2]);
+  }
+  return null;
+}
+
+/**
+ * 判断当前网页是否需要重定向
+ */
+const checkRedirect = async () => {
+  const codeExist = window.location.search.includes('code');
+  // 判断是否需要重定向
+  if (!codeExist) {
+    window.location.replace(generateOAuthUrl(config));
+  }
+  const code = getQueryString('code');
+  // const res = await getTokenByCode();
+  // return res;
+
+  // storage.set('nqToken', res.token);
+};
+
+export {
+  checkRedirect,
+}
