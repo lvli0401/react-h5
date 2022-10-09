d3692ede57ad38655bb8cae513150f7bdf1f0b26
currycheng777 <currycheng@163.com>
2022-10-09 09:45:54 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { checkRedirect } from '@utils/index'
-import storage from '@utils/storage'
 
 const App: React.FC<any> = () => {
-  const isLogin = storage.get('userInfo')?.id;
-  if (!isLogin) {
-    checkRedirect();
-    return <></>
-  }
   return (
     <div>
       <Outlet />
